a7fc985ba8d2beaf4c7d9afb3dbe8f10eaa1507a
baratamudha <randiachmad32@gmail.com>
2025-03-27 12:33:14 +0700

    1st commit

--- app/src/main/java/com/example/todolistapp/taskmanager/data/Task.kt
+++ app/src/main/java/com/example/todolistapp/taskmanager/data/Task.kt
@@ -15,4 +15,26 @@ val taskList = listOf(
         startTime = "10.00",
         endTime = "11.00"
     ),
+    Task(
+        2,
+        "Clean Kitchen",
+        "Wash dishes, wipe counters, and mop the floor",
+        "11:30",
+        "12:30"
+    ),
+    Task(
+        3,
+        "Vacuum Living Room",
+        "Clean carpets and furniture",
+        "13:00",
+        "14:00"
+    ),
+    Task(
+        4,
+        "Water Plants",
+        "Water indoor and outdoor plants",
+        "15:00",
+        "16:00"
+    ),
 )
+
--- app/src/main/java/com/example/todolistapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/todolistapp/ui/theme/Color.kt
@@ -2,12 +2,12 @@ package com.example.todolistapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
+val Purple200 = Color(0xFFC2DEDC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val LightPurple = Color(0xFFF1F4FF)
-val LightGreen = Color(0xFFEFFCEF)
+val LightGreen = Color(0xFF95D4C6)
 val LightBlue = Color(0xFFE6F5FB)
 val Orange = Color(0xFFF69A0E)
 val LightGray = Color(0xFFADADAD)
